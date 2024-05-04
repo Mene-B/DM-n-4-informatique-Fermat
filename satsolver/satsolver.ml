19d36196d44fb3c176f993cb9fa4419dafb4cca6
Mene-B <82470140+Mene-B@users.noreply.github.com>
2024-05-04 08:38:14 +0200

    Update satsolver.ml

--- satsolver/satsolver.ml
+++ satsolver/satsolver.ml
@@ -1,1 +1,10 @@
+let test () = 
+    assert (1=1);
+    print_string "Tous les tests ont réussi \n"
 
+let main () = 
+    if (Array.length Sys.argv < 2) then failwith "Veuillez rentrer un argument\n" else 
+    if (Sys.argv.(1) = "test") then test () else 
+    print_string Sys.argv.(1); print_string "\n\n"
+
+let _ = main ()
