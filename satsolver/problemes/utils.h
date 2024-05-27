dced794e9c2960005b00f29dcaede1347c88ee4d
arthurc31 <95419853+arthurc31@users.noreply.github.com>
2024-05-27 17:27:51 +0200

    Update utils.h

---
+++ satsolver/problemes/utils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+//prend en entre une liste l de fromules atomiqes de taille totale et fabrique la disjonction de ces formules
+char* au_moins_une (char** l, int n);
