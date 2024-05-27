814345388ad3f4e2679304b0f0e1003ca6aa307e
arthurc31 <95419853+arthurc31@users.noreply.github.com>
2024-05-27 21:38:44 +0200

    Update utils.h

--- satsolver/problemes/utils.h
+++ satsolver/problemes/utils.h
@@ -5,5 +5,14 @@
 #include <assert.h>
 #include <string.h>
 
-//prend en entre une liste l de fromules atomiqes de taille totale n et fabrique la disjonction de ces formules
+//prend  une liste l de fromules atomiqes de taille n et fabrique la disjonction de ces formules
 char* au_moins_une (char** l, int n);
+
+//renvoie la négation de la formule atomique l.
+char* non(char* l);
+
+//prend une liste l de fromules atomiqes de taille n et fabrique la conjonction de ces formules
+char* toutes_vraies(char** l, int n);
+
+//prend une liste l de formules atomiques de taille n et renvoie une formule exprimant le fait qu'au plus une est vraie
+char* au_moins_une(char** l, int n);
