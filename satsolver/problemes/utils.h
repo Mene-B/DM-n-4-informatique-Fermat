88e4f813f72b403a4694e90d8905737577afd699
Mene-B <82470140+Mene-B@users.noreply.github.com>
2024-05-27 21:30:32 +0200

    Update utils.h
    modification dans un commentaire de au_moins_une

--- satsolver/problemes/utils.h
+++ satsolver/problemes/utils.h
@@ -5,5 +5,5 @@
 #include <assert.h>
 #include <string.h>
 
-//prend en entre une liste l de fromules atomiqes de taille totale et fabrique la disjonction de ces formules
+//prend en entre une liste l de fromules atomiqes de taille totale n et fabrique la disjonction de ces formules
 char* au_moins_une (char** l, int n);
