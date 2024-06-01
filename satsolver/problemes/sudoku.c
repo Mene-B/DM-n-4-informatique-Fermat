e7a358fc010aa7a543ee724b9b2ecda078451e28
Mene-B <82470140+Mene-B@users.noreply.github.com>
2024-06-01 16:23:54 +0200

    Update sudoku.c

--- satsolver/problemes/sudoku.c
+++ satsolver/problemes/sudoku.c
@@ -327,5 +327,20 @@ void test(){
 }
 
 int main(){
-    gen_formule_n_sudoku(2,"2_sudoku.txt");
+    test();
+    assert(argc == 2);
+
+    int taille_string = strlen(argv[1]);
+    char* file_out = malloc((12+taille_string)*sizeof(char));
+    strcat(file_out,argv[1]);
+    strcat(file_out,"_sudoku.txt\0");
+
+    int n=0;
+    int p=1;
+    for (int i=taille_string-1; i>-1; i--){
+        n+=(argv[1][i]-'0')*p;
+        p=10*p;
+    }
+    gen_formule_n_sudoku(n,file_out);
+    printf("fichier %s créé\n", file_out);
 }
